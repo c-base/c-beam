a56e943799754ac9ed141d56e761aefff0270767
sMiLe <smile@c-base.org>
2011-04-30 22:02:42 +0200

    its working

--- plugins/hubelmeter.py
+++ plugins/hubelmeter.py
@@ -22,24 +22,45 @@ import re
 RE_PRONOUN = re.compile(r'jemand|irgendwer|man|einer')
 RE_CONJUNCTIVE = re.compile(r'sollte|m.sste|k.nnte|h.tte')
 
+## HubelItem class
+
+class HubelItem(PlugPersist):
+
+    def __init__(self, name, default={}):
+        PlugPersist.__init__(self, name)
+        self.data.name = name
+        self.data.rowcount = self.data.rowcount or 0.0
+        self.data.hubelcount = self.data.hubelcount or 0.0
+
+    def hubel(self):
+        print self.data.hubelcount
+        print  self.data.rowcount
+        return float(self.data.hubelcount) / float(self.data.rowcount)
+      
 ## hubelmeter-precondition
 
 def prehubelmeter(bot, event):
      # if not event.iscmnd(): return False
      # we want to catch all the ++ and to avoid cheating
     if event.userhost in bot.ignore: return False
+    if len(event.txt) > 0 and event.txt[0] == '!': return False
     pronoun = re.search(RE_PRONOUN, event.txt)
     conjunctive = re.search(RE_CONJUNCTIVE, event.txt)
 
     print pronoun
     print conjunctive
+    i = HubelItem(getuser(event))
+    # increase linecounter only
+    i.data.rowcount += 1.0
     if pronoun and conjunctive:
-        # increase hubelcounter and linecounter
+        # increase hubelcounter
+        i.data.hubelcount += 1.0
+        i.save()
         print "hubel!!1elf"
         event.reply('HUBEL!!1elf')
         return True
     else:
-        # increase linecounter only
+        i.save()
         return False
 
 ## hubelmeter-callbacks
@@ -52,15 +73,23 @@ callbacks.add('MESSAGE', hubelmetercb, prehubelmeter)
 callbacks.add('CONSOLE', hubelmetercb, prehubelmeter)
 callbacks.add('CONVORE', hubelmetercb, prehubelmeter)
 
+def getuser(event):
+    return event.nick
+
 ## hubelmeter command
 
 def handle_hubelmeter(bot, event):
     """ show hubelmeter of item. """
-    if not event.rest: event.missing("<what>") ; return
-    k = event.rest.lower()
-    item = KarmaItem(event.channel.lower() + "-" + k)
-    if item.data.count: event.reply("hubelmeter of %s is %s" % (k, item.data.count))
-    else: event.reply("%s doesn't have hubelmeter yet." % k)
+    if not event.rest:
+        item = getuser(event)
+        #event.missing("<what>") ; return
+    else:
+        item = event.rest.lower()
+
+    print item
+    i = HubelItem(item)
+    print i.data.hubelcount
+    event.reply("%s has %f hubel." % (item, i.hubel()))
 
 cmnds.add('hubelmeter', handle_hubelmeter, ['OPER', 'USER', 'GUEST'])
 examples.add('hubelmeter', 'show hubelmeter', 'hubelmeter jsb')
