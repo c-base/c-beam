1621ba317dc6a60de5af8e1ba4a0a7dc3502af84
smile <smile@c-base.org>
2011-06-19 03:02:01 +0200

    usings args as text for tts

--- c_out/ttsclient.py
+++ c_out/ttsclient.py
@@ -1,10 +1,12 @@
 #!/usr/bin/env python
 
 import jsonrpclib
+import sys
 
 jsonrpclib.config.version = 1.0
 server = jsonrpclib.Server('http://10.0.1.13:1775')
 
 #print server.rgb(255, 0, 0)
-print server.tts('julia', 'foo')
+print " ".join(sys.argv[1:])
+print server.tts('julia', " ".join(sys.argv[1:]))
 print server.voices()
