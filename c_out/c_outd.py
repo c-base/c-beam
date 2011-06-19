3cdce3b926bf370f58bbddc7c14808df7106f03d
sMiLe <smile@c-base.org>
2011-06-19 22:32:33 +0200

    copied from ttsd.py, added method stubs for some future methods

---
+++ c_out/c_outd.py
@@ -0,0 +1,95 @@
+#! /usr/bin/python
+
+import httplib, urllib, random, re, os, sys, time
+from jsonrpclib.SimpleJSONRPCServer import SimpleJSONRPCServer
+
+player = 'mpg123'
+sampledir = '/mnt/datengrab/00_audio/c_out'
+sampledir = '/tmp'
+password = '0g7znor2aa'
+
+thevoices = ['lucy', 'peter', 'rachel', 'heather', 'kenny', 'laura', 'nelly', 'ryan', 'julia', 'sarah', 'klaus']
+
+def main():
+    server = SimpleJSONRPCServer(('0.0.0.0', 1775))
+
+    server.register_function(tts, 'tts')
+    server.register_function(r2d2, 'r2d2')
+    server.register_function(play, 'play')
+    server.register_function(volume, 'volume')
+    server.register_function(voices, 'voices')
+    server.serve_forever()
+
+def voices():
+    return thevoices
+
+def tts(voice, text):
+    if voice in thevoices:
+        voice = '%s22k' % voice
+    else:
+        voice = 'lucy22k'
+
+    pitch = 100
+    speed = 180
+    if not text.endswith("."): text = "%s." % (text,)
+    filename = '%s/%s_%s_%d_%d.mp3' % (sampledir, urllib.quote(text.lower()), voice, pitch, speed)
+    textparam = '\\vct=%d\\ \\spd=%d\\ %s' % (pitch, speed, text)
+
+    # check whether we have a cached version of the the file
+    if os.path.isfile(filename):
+        play(filename)
+    else:
+        params = urllib.urlencode({
+            'cl_env': 'FLASH_AS_3.0',
+            'req_asw_type': 'INFO',
+            'req_voice': voice,
+            'req_timeout': '120',
+            'cl_vers': '1-30',
+            'req_snd_type': '',
+            'req_text': textparam,
+            'cl_app': 'PROD',
+            'cl_login': 'ACAPELA_BOX',
+            'prot_vers': '2',
+            'req_snd_id': '0_0_84%s88' % random.randint(0, 32767),
+            'cl_pwd': password
+        })
+
+        headers = {"Content-type": "application/x-www-form-urlencoded",
+                  "Accept": "text/plain"}
+        conn = httplib.HTTPConnection("vaassl3.acapela-group.com")
+        conn.request("POST", "/Services/AcapelaBOX/0/Synthesizer", params, headers)
+        response = conn.getresponse()
+        data = response.read()
+        conn.close()
+
+        url = re.compile('http://.*\.mp3').search(data).group()
+
+        mysock = urllib.urlopen(url)
+        fileToSave = mysock.read()
+        oFile = open('%s' % filename,'wb')
+        oFile.write(fileToSave)
+        oFile.close
+
+        play(filename)
+
+def r2d2(text):
+    return "not implemented"
+
+def festival(text):
+    return "not implemented"
+
+def volume(vol):
+    return "not implemented"
+
+def play(filename):
+    # TODO: check if filename contains /, otherwise use default directory
+    print '%s %s' % (player, filename)
+    if player == 'mplayer':
+        print 'mplayer -af volume=+10 -really-quiet -ao esd %s >/dev/null' % filename
+        os.system('mplayer -af volume=+10 -really-quiet -ao esd %s >/dev/null' % filename)
+    else:
+        os.system('%s %s' % (player, filename))
+
+if __name__ == "__main__":
+    main()
+
