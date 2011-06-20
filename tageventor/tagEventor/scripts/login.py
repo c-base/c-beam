1ffcfe4d00230479b4d81c195fad5f124fc594e9
smile <smile@c-base.org>
2011-06-20 10:33:01 +0200

    fixed tegEventor scripts

--- tageventor/tagEventor/scripts/login.py
+++ tageventor/tagEventor/scripts/login.py
@@ -9,38 +9,41 @@ server = jsonrpclib.Server('http://10.0.1.13:1775')
 
 userdir = "/home/c-beam/users"
 
+logindelta = 10
+
 
 def tts(voice, text):
     print server.tts(voice, text)
 
 
-def login(user, logindelta, timeoutdelta):
+def log(message):
+    print "%s: %s" % (datetime.datetime.now(),message)
+
+def login(user, timeoutdelta):
+    log("called login for %s" % user)
     if user == "unknown":
         return "login"
     userfile = '%s/%s' % (userdir, user)
     if os.path.isfile(userfile):
-        print userfile
         timestamps = eval(open(userfile).read())
-        print timestamps
         now = int(datetime.datetime.now().strftime("%Y%m%d%H%M%S"))
         if timestamps[0] - now > 0:
            # multiple logins, ignore
-           print "multiple logins, ignoring"
+           log("multiple logins, ignoring")
            return "ignore"
         else:
            #logout
+           log("%s logged out" % user)
            os.rename(userfile, "%s.logout" % userfile)
            return "logout"
     else:
         if os.path.isfile("%s.logout" % userfile):
            return "ignore"
         else:
-           logints = datetime.datetime.now() + datetime.timedelta(minutes=logindelta)
+           logints = datetime.datetime.now() + datetime.timedelta(seconds=logindelta)
            timeoutts = datetime.datetime.now() + datetime.timedelta(minutes=timeoutdelta)
-           #eta = int(foo.strftime("%Y%m%s%H%M"))
            expire = [int(logints.strftime("%Y%m%d%H%M%S")), int(timeoutts.strftime("%Y%m%d%H%M%S"))]
-           print expire
            f = open(userfile, 'w')
            f.write(str(expire))
-
+           log("%s logged in" % user)
            return "login"
