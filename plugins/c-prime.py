9b5099dc619978a2579b0c46e51167a8ff417694
smile <smile@c-base.org>
2011-07-31 05:40:42 +0200

    .

---
+++ plugins/c-prime.py
@@ -0,0 +1,4 @@
+praesens_inikativ = ['bin', 'bist', 'ist', 'sind', 'seid', 'sind']
+
+http://canoo.net/services/Controller?dispatch=inflection&input=sein&features=%28Cat+V%29%28Aux+sein%29&country=D&lookup=caseInSensitive
+
