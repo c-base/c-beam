a69815cb3d5a63cdd740a1c5f1a3f6b74e862eb9
smile <smile@c-base.org>
2011-08-05 00:55:37 +0200

    another fix for c-base/c-beam pronounciation

--- c_out/c_outd.py
+++ c_out/c_outd.py
@@ -113,8 +113,8 @@ def acapela(voice, text):
     text = text.replace('$','Dollar')
     if (voice in ['julia', 'sarah', 'klaus']):
         #text = text.replace('c-base','zieh baejs')
-        text = text.replace('c-base','ssiebays')
-        text = text.replace('c-beam','ssiebiem')
+        text = text.replace('c-base','ziebays')
+        text = text.replace('c-beam','ziebiem')
         text = text.replace('c3pb', 'zeh drei p b')
 
     if voice.find('22k') == -1:
