7d4c7eabaf8a480c99da18c4a9002ccfc5bae444
smile <smile@c-base.org>
2011-08-31 19:35:54 +0200

    moved config stuff to a separate file

---
+++ c-beamd/cbeamdcfg.py
@@ -0,0 +1,13 @@
+# CONFIG BEGIN ######################################################################
+userdir = "/home/c-beam/users"
+vuserdir = "/home/c-beam/vusers"
+datafile = "/home/c-beam/c-beam.data"
+c_outurl = "http://10.0.1.13:1775"
+logfile = '/home/smile/c-beam.log'
+logindelta = 30
+eta_timeout = 120
+etd_timeout = 120
+timeoutdelta = 600
+ttsgreeting = "hallo %s, willkommen an bord"
+ttsenabled = 1
+# CONFIG END ########################################################################
