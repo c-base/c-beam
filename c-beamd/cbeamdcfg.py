ed60cbb98e3a1f3327aad3ae2d19980b57bf01bd
smile <smile@c-base.org>
2011-10-05 11:01:21 +0200

    added sampledir config directive

--- c-beamd/cbeamdcfg.py
+++ c-beamd/cbeamdcfg.py
@@ -3,6 +3,7 @@ userdir = "/home/c-beam/users"
 vuserdir = "/home/c-beam/vusers"
 datafile = "/home/c-beam/c-beam.data"
 c_outurl = "http://10.0.1.13:1775"
+monitorurl = "http://127.0.0.1:9090"
 logfile = '/home/smile/c-beam.log'
 logindelta = 30
 eta_timeout = 120
@@ -10,4 +11,5 @@ etd_timeout = 120
 timeoutdelta = 600
 ttsgreeting = "hallo %s, willkommen an bord"
 ttsenabled = 1
+sampledir = "/home/c-beam"
 # CONFIG END ########################################################################
