950284d554f5bc8f1c56c268a644f143dbb6aacc
baccenfutter <baccenfutter@c-base.org>
2012-01-07 21:42:47 +0100

    added nonsmoker plugin

---
+++ nonsmoking/nonsmoking.py
@@ -0,0 +1,17 @@
+from jsb.utils.exception import handle_exception
+from jsb.lib.commands import cmnds
+from jsb.lib.examples import examples
+
+import datetime
+
+firstday = datetime.datetime(2011, 12, 6, 0, 0, 0)
+
+def pose_as_nonsmoker(bot, ievent):
+    now = datetime.datetime.now()
+    diff = now - firstday
+    days = diff.days
+    bot.say('#c-base', "baccenfutter ist jetzt seit %i Tagen Nichtraucher." % days)
+
+cmnds.add("smoke", pose_as_nonsmoker, ['OPER', 'USER', 'GUEST'])
+example.add('smoke', 'Wie lange ist baccenfutter schon Nichtraucher?')
+
