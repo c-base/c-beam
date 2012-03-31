de0a81ea1d331d0d39f0b511f8cff71c245b6754
smile <smile@c-base.org>
2012-03-31 18:59:22 +0200

    .

--- c-beamd/cbeamdcfg.py
+++ c-beamd/cbeamdcfg.py
@@ -2,7 +2,7 @@
 userdir = "/home/c-beam/users"
 vuserdir = "/home/c-beam/vusers"
 datafile = "/home/c-beam/c-beam.data"
-c_outurl = "http://10.0.1.13:1775"
+c_outurl = "http://10.0.1.123:1775"
 monitorurl = "http://127.0.0.1:9090"
 logfile = '/home/smile/c-beam.log'
 logindelta = 30
