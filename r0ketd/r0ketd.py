d3873297d3db51de7ff8b435e13c6d08828691ea
smile <smile@c-base.org>
2012-05-04 13:07:10 +0200

    added r0ketseen call

--- r0ketd/r0ketd.py
+++ r0ketd/r0ketd.py
@@ -7,7 +7,7 @@ from jsonrpclib.SimpleJSONRPCServer import SimpleJSONRPCServer
 logindelta = 10
 timeoutdelta = 240
 
-r0ketmap = {'95471AAF': 'baccenfutter'}
+r0ketmap = {'95471AAF': 'baccenfutter', 'TEST': 'TEST', '1B3A8845': 'MM'}
 
 jsonrpclib.config.version = 1.0
 cbeam = jsonrpclib.Server('http://10.0.1.27:4254')
@@ -35,9 +35,12 @@ def main():
         # RECV 95471AAF: B2007A (100.00)
         result = re.search('RECV (.*): (.*) \((.*)\)', line) 
         if result != None:
-            print 'r0ket %s detected, logging in %s' % (result.group(1), r0ketmap[result.group(1)])
             if result.group(1) in r0ketmap.keys():
+                print 'r0ket %s detected, logging in %s' % (result.group(1), r0ketmap[result.group(1)])
+                cbeam.r0ketSeen(result.group(1), sensor, result.group(2), result.group(3))
                 result = cbeam.login(r0ketmap[result.group(1)])
+            else:
+                print 'saw unknown r0ket: %s' % result.group(1)
         sys.stdout.flush()
 
 
