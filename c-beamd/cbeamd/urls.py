6089d7c6e96f3f6c04d1a8f7ac8ab0704f2f0481
smile <smile@c-base.org>
2013-03-20 16:47:28 +0100

    updated urls

--- c-beamd/cbeamd/urls.py
+++ c-beamd/cbeamd/urls.py
@@ -17,7 +17,8 @@ user_dict = {
 
 urlpatterns = patterns('',
     # Examples:
-    url(r'^$', 'cbeamd.views.index'),
+    #url(r'^$', 'cbeamd.views.index'),
+    url(r'^$', 'cbeamd.views.user_online'),
     #url(r'^cbeamd/', include('cbeamd.foo.urls')),
 
     # Uncomment the admin/doc line below to enable admin documentation:
@@ -37,10 +38,18 @@ urlpatterns = patterns('',
     url(r'^user/logout/(?P<user>.+)$', 'cbeamd.views.logout'),
     url(r'^login$', 'cbeamd.views.auth_login'),
     url(r'^logout$', 'cbeamd.views.auth_logout'),
-    url(r'^missions/(?P<object_id>\d+)/$', 'django.views.generic.list_detail.object_detail', dict(mission_dict, template_name='cbeamd/mission_detail.django'), mission_dict),
+    #url(r'^missions/(?P<object_id>\d+)/$', 'django.views.generic.list_detail.object_detail', dict(mission_dict, template_name='cbeamd/mission_detail.django'), mission_dict),
     url(r'^missions$', 'cbeamd.views.mission_list'),
     url(r'^missions/(?P<object_id>\d+)/edit$', 'cbeamd.views.edit_mission'),
-    url(r'^stripe/$', 'cbeamd.views.stripe_view'),
+    url(r'^artefacts$', 'cbeamd.views.artefact_list_web'),
+    url(r'^events$', 'cbeamd.views.event_list_web'),
+    #url(r'^stripe/$', 'cbeamd.views.stripe_view'),
+    url(r'^control/set_stripe_pattern/(?P<pattern_id>\d+)/$', 'cbeamd.views.set_stripe_pattern_web'),
+    url(r'^control/set_stripe_speed/(?P<speed>\d+)/$', 'cbeamd.views.set_stripe_speed_web'),
+    url(r'^control$', 'cbeamd.views.control'),
+    url(r'^control/softwareendlager$', 'cbeamd.views.hwstorage_web'),
+    url(r'^c_out$', 'cbeamd.views.c_out_web'),
+    url(r'^c_out/play/(?P<sound>.+)$', 'cbeamd.views.c_out_play_web'),
 )
 
 urlpatterns += patterns('', (r'^rpc/', jsonrpc_site.dispatch))
