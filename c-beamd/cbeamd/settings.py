6965b5260b892b1d9ec896bbd1b69b5fa42c82bb
cmile <github@cmile.de>
2015-12-05 01:53:23 +0100

    bug fixes for 1.9

--- c-beamd/cbeamd/settings.py
+++ c-beamd/cbeamd/settings.py
@@ -67,7 +67,7 @@ STATIC_URL = '/static/'
 
 # Additional locations of static files
 STATICFILES_DIRS = [
-    '/home/smile/c-beam/c-beamd/cbeamd/static'
+    '/home/c-beam/c-beam/c-beamd/cbeamd/static'
     # Put strings here, like "/home/html/static" or "C:/www/django/static".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
@@ -154,7 +154,7 @@ LOGGING = {
         'file': {
             'level': 'DEBUG',
             'class': 'logging.FileHandler',
-            'filename': '/home/smile/c-beam-debug.log',
+            'filename': '/home/c-beam/c-beam-debug.log',
         },
     },
     'loggers': {
