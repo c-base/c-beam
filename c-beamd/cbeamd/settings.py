abf97fb0ab5cf345a7e60ea297b2dcf0102f07f6
smile <smile@c-base.org>
2026-09-06 11:30:06 +0200

    remove smart_text monkeypatch

--- c-beamd/cbeamd/settings.py
+++ c-beamd/cbeamd/settings.py
@@ -3,10 +3,6 @@ import os
 from django.urls import reverse_lazy
 from decouple import config
 
-import django
-from django.utils.encoding import smart_str
-django.utils.encoding.smart_text = smart_str
-
 # Django settings for cbeamd project.
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
